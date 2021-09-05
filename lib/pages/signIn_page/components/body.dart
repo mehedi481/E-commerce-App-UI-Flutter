54914e4b38f2b99471de6aa6f5cfcbb6dc9606f9
Mehedi <mehedihasanmax95@gmail.com>
2021-09-05 22:38:13 +0600

    text

--- lib/pages/signIn_page/components/body.dart
+++ lib/pages/signIn_page/components/body.dart
@@ -9,13 +9,41 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Column(
-      children: [
-        Text(
-          "Welcome Back",
-          style: TextStyle(fontSize: getProportionateScreenWidth(28)),
-        )
-      ],
+        child: SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          Text(
+            "Welcome Back",
+            style: TextStyle(
+              fontSize: getProportionateScreenWidth(28),
+            ),
+          ),
+          Text(
+            "Sign in with your email and password or Continue with your social network",
+            textAlign: TextAlign.center,
+          ),
+          CustomTextFormField()
+        ],
+      ),
     ));
+  }
+}
+
+class CustomTextFormField extends StatefulWidget {
+  const CustomTextFormField({Key? key}) : super(key: key);
+
+  @override
+  _CustomTextFormFieldState createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 320,
+      width: 320,
+      color: Colors.black,
+    );
   }
 }
