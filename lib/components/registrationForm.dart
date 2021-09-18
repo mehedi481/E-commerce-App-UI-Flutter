6f5f19fa63d24d007d044e2c17ebbb2fd0a14434
Mehedi <mehedihasanmax95@gmail.com>
2021-09-18 15:29:44 +0600

    registration complete

---
+++ lib/components/registrationForm.dart
@@ -0,0 +1,46 @@
+import 'package:e_commerce_app_ui/helpers/size_config/size_config.dart';
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({Key? key}) : super(key: key);
+
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextFormField(
+          decoration: inputDecoration(
+            text: "Email",
+            hintText: "Enter your email",
+            icon: Icon(Icons.email),
+          ),
+        ),
+        SizedBox(height: getProportionateScreenHeight(25)),
+        TextFormField(
+          decoration: inputDecoration(
+            text: "Password",
+            hintText: "Enter your password",
+            icon: Icon(Icons.lock),
+          ),
+        ),
+        SizedBox(height: getProportionateScreenHeight(25)),
+        TextFormField(
+          decoration: inputDecoration(
+            text: "Confirm Password",
+            hintText: "Re-enter your password",
+            icon: Icon(Icons.lock),
+          ),
+        ),
+        SizedBox(height: getProportionateScreenHeight(25)),
+        defaultButton(text: "Continue", onPress: () {}),
+      ],
+    );
+  }
+}
--- lib/pages/reg_account.dart/components/reg_body.dart
+++ lib/pages/reg_account.dart/components/reg_body.dart
@@ -1,6 +1,6 @@
+import 'package:e_commerce_app_ui/components/registrationForm.dart';
 import 'package:e_commerce_app_ui/components/socialCard.dart';
 import 'package:e_commerce_app_ui/helpers/size_config/size_config.dart';
-import 'package:e_commerce_app_ui/main.dart';
 import 'package:flutter/material.dart';
 
 class RegisterBody extends StatelessWidget {
@@ -8,81 +8,56 @@ class RegisterBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        children: [
-          Text(
-            "Registrer Account",
-            style: TextStyle(
-              fontSize: getProportionateScreenWidth(25),
-              fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(15),
             ),
-          ),
-          Text(
-            "Complete your details or continue \n with social media",
-            style: TextStyle(
-              fontSize: getProportionateScreenWidth(16),
+            Text(
+              "Registrer Account",
+              style: TextStyle(
+                fontSize: getProportionateScreenWidth(25),
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            textAlign: TextAlign.center,
-          ),
-          RegisterForm(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SocialCard(icon: Icons.email, press: () {}),
-              SocialCard(icon: Icons.facebook, press: () {}),
-              SocialCard(icon: Icons.email, press: () {}),
-            ],
-          ),
-          Text(
-            "By continuing your confirm that you agree\n with our Term and Condition",
-            textAlign: TextAlign.center,
-          )
-        ],
+            SizedBox(
+              height: getProportionateScreenHeight(15),
+            ),
+            Text(
+              "Complete your details or continue \n with social media",
+              style: TextStyle(
+                fontSize: getProportionateScreenWidth(16),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: ScreenSize.height! * 0.09,
+            ),
+            RegisterForm(),
+            SizedBox(
+              height: getProportionateScreenHeight(25),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SocialCard(icon: Icons.email, press: () {}),
+                SocialCard(icon: Icons.facebook, press: () {}),
+                SocialCard(icon: Icons.email, press: () {}),
+              ],
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(25),
+            ),
+            Text(
+              "By continuing your confirm that you agree\n with our Term and Condition",
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
       ),
-    );
-  }
-}
-
-class RegisterForm extends StatefulWidget {
-  const RegisterForm({Key? key}) : super(key: key);
-
-  @override
-  _RegisterFormState createState() => _RegisterFormState();
-}
-
-class _RegisterFormState extends State<RegisterForm> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-          decoration: inputDecoration(
-            text: "Email",
-            hintText: "Enter your email",
-            icon: Icon(Icons.email),
-          ),
-        ),
-        SizedBox(height: getProportionateScreenHeight(25)),
-        TextFormField(
-          decoration: inputDecoration(
-            text: "Password",
-            hintText: "Enter your password",
-            icon: Icon(Icons.lock),
-          ),
-        ),
-        SizedBox(height: getProportionateScreenHeight(25)),
-        TextFormField(
-          decoration: inputDecoration(
-            text: "Confirm Password",
-            hintText: "Re-enter your password",
-            icon: Icon(Icons.lock),
-          ),
-        ),
-        SizedBox(height: getProportionateScreenHeight(25)),
-        defaultButton(text: "Continue", onPress: () {}),
-      ],
     );
   }
 }
